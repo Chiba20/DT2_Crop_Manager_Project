977c62d15be880a120acfc9633f8497d0c42f9ad
Nabiha4199 <zda23b018@iitmz.ac.in>
2025-12-14 00:06:58 +0300

    Add files via upload
    update of the prediction.py file

--- backend/yield_prediction/predictor.py
+++ backend/yield_prediction/predictor.py
@@ -26,3 +26,28 @@ def load_training_data():
         y.append(yield_amount)
 
     return np.array(X), np.array(y)
+
+def predict_yield(area, planting_date):
+    X, y = load_training_data()
+
+    if len(X) < 2:
+        return None, "Not enough data"
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    month = datetime.strptime(planting_date, "%Y-%m-%d").month
+    prediction = model.predict([[area, month]])[0]
+
+    return prediction, categorize_yield(prediction, y)
+
+def categorize_yield(prediction, all_yields):
+    low = np.percentile(all_yields, 33)
+    high = np.percentile(all_yields, 66)
+
+    if prediction < low:
+        return "Low"
+    elif prediction < high:
+        return "Medium"
+    else:
+        return "High"
